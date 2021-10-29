26bba1b4ca4dd281876009fb0169e20bdcd100ec
Samikshya <write2samikshyakar@gmail.com>
2021-10-29 19:28:26 +0530

    Add project files.

---
+++ WebApi_Samu_Trial1/Controllers/SamuShopController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessSamuTrial;
+
+
+namespace WebApi_Samu_Trial1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SamuShopController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<SamuShopEmployee> GetAllShopkeeperDetails()
+        {
+
+            using(SamuShopEmployeeDBEntities samuShopEmployeeDBEntities = new SamuShopEmployeeDBEntities())
+            {
+                return samuShopEmployeeDBEntities.SamuShopEmployees.ToList();
+
+            }
+        }
+
+
+       
+        public SamuShopEmployee GetEmployeebyID(int ssID)
+        {
+            using(SamuShopEmployeeDBEntities samuShopEmployeeDBEntities= new SamuShopEmployeeDBEntities())
+            {
+
+                return samuShopEmployeeDBEntities.SamuShopEmployees.FirstOrDefault(ssed => ssed.ID == ssID);
+
+            }
+
+        }
+
+
+
+
+
+    }
+}
---
+++ WebApi_Samu_Trial1/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessSamuTrial;
+////using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApi_Samu_Trial1
+{
+    public class Startup
+    {
+
+        private IConfiguration _config;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _config = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextPool<SamuShopEmployeeDBEntities>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
